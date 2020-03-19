9051a0fade64d4a86ea0e3eea4eea4bc15e57983
= <=>
2020-03-19 17:34:11 -0400

    import bootstrap from node_modules into app.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,20 @@
 import * as React from "react";
 import "./App.css";
 import io from 'socket.io-client';
+import Container from "react-bootstrap/Container";
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.socket = io("http://localhost:80");
-    this.state = {
-      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    };
-  }
 
-  handleClick = event => {
-    const number = event.target.innerText;
-    this.socket.emit("click", number);
-  };
-
-  styles = () => { 
-    return {
-      width: "50px",
-      height: "50px",
-      padding: "25px",
-      backgroundColor: `rgb(
-      ${Math.random() * 255},
-      ${Math.random() * 255},
-      ${Math.random() * 255}
-    )`,
-      textAlign: 'center'
-    };
-  }
   render() {
 
     return (
-      <div>
-        <h1 onClick={e => this.handleClick(e)}>Hello</h1>
-      </div>
+      <Container>
+        <Jumbotron variant = "danger">
+      <h1>Bootstrap is working!</h1>
+        </Jumbotron>
+  </Container>
     )
   }
 }
