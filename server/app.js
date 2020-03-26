5796d14780670e8c677c278ae795e5d971734e32
= <=>
2020-03-26 15:04:58 -0400

    create new user query and handle response

--- server/app.js
+++ server/app.js
@@ -34,7 +34,14 @@ server.listen(80);
 console.log('server started')
 
 app.post('/signup', function (req, res) {
-    // Handle signup
+  const db = req.app.get('db');
+  const { username, password } = req.body;
+  db.create_user(username, password)
+    .then(user => { 
+      res.status(200).send(user)
+    }).catch(err => { 
+      res.status(500).send(err)
+    })
 });
 
 // Register the index route of your app that returns the HTML file
